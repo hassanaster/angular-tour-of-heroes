a792622f74558c7c8a665f5e406b315f6f14c139
hassanaster <hassanaster2@gmail.com>
2018-08-05 22:48:33 +0200

    Actividad 02 - Punto 04 - Click event on heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,16 +7,26 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
-  constructor() { }
 
-  ngOnInit() {
-  }
+export class HeroesComponent implements OnInit {
+    
+    heroes = HEROES;
+    
+    selectedHero: Hero;
+
+    hero: Hero = {
+    	id: 1,
+    	name: 'Windstorm'
+    };
+    
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onSelect(hero: Hero): void {
+  	  this.selectedHero = hero;
+     }
 
 }
 
