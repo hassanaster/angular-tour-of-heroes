6b16f2bf148ddc5b13e99ea9cb7291acf0f92256
hassanaster <hassanaster2@gmail.com>
2018-08-11 14:51:40 +0200

    Actividad 2 - 7. Routing - Routable HeroDetailComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { Hero } from './hero';
 
 
 
@@ -19,11 +20,17 @@ export class HeroService
 
   
 
-	getHeroes(): Observable<Hero[]> {
-  		// TODO: send the message _after_ fetching the heroes
-  		this.messageService.add('HeroService: fetched heroes');
-  		return of(HEROES);
-	}
-	constructor(private messageService: MessageService) { }
+	  getHeroes(): Observable<Hero[]> {
+    		// TODO: send the message _after_ fetching the heroes
+    		this.messageService.add('HeroService: fetched heroes');
+    		return of(HEROES);
+           }
+ 
+  	   getHero(id: number): Observable<Hero> {
+    		// TODO: send the message _after_ fetching the hero
+    		this.messageService.add(`HeroService: fetched hero id=${id}`);
+    		return of(HEROES.find(hero => hero.id === id));
+  	   }
+	   constructor(private messageService: MessageService) { }
 
 }
