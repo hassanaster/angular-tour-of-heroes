20869708ba4e3f7a342405513f39d5d263135ac4
hassanaster <hassanaster2@gmail.com>
2018-08-11 07:04:17 +0200

    Actividad 2 - Routing, login branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,22 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { MessagesComponent } from './messages/messages.component';
 
+import { AppRoutingModule } from './/app-routing.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
